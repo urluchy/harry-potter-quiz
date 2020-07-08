f2a818ce4259eecdcfe127d21cff1b0cdd62ca91
Ursula Okolie <45493240+urluchy@users.noreply.github.com>
2020-07-08 21:02:47 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+    <Quiz :movies="movies"/>
+  </div>
+</template>
+
+<script>
+import Quiz from "./components/Quiz.vue";
+
+export default {
+  name: "App",
+  components: {
+    Quiz
+  },
+  data() {
+    return {
+      movies: [
+        "Harry Potter and the Philosopher's Stone",
+        "Harry Potter and the Chamber of Secrets",
+        "Harry Potter and the Prisoner of Azkaban",
+        "Harry Potter and the Goblet of Fire",
+        "Harry Potter and the Order of the Phoenix",
+        "Harry Potter and the Half-Blood Prince",
+        "Harry Potter and the Deathly Hallows - Part 1",
+        "Harry Potter and the Deathly Hallows - Part 2"
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+html {
+  height: 100%;
+}
+body {
+  height: 100%;
+  background: #020815; /* Black */
+  color: #eee; /* Gray */
+}
+
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  margin-top: 60px;
+}
+</style>
+
---
+++ src/components/Quiz.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <img
+      src="https://media0.giphy.com/media/Bh3YfliwBZNwk/giphy.gif?cid=3640f6095c852266776c6f746fb2fc67"
+      alt="A castle at the top of a mountain in a gray day with thunder."
+    >
+    <h1 class="quiz-heading">How Well Do You Know the Harry Potter Movies?</h1>
+    <button class="quiz-button" @click="initQuizStage">Start Quiz</button>
+
+    <ul>
+      <li v-for="movie in movies" :key="movie">{{ movie }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    movies: {
+      type: Array,
+      required: true
+    }
+  },
+  name: "quiz",
+  methods: {
+    initQuizStage() {
+      console.log("Start the quiz...");
+    }
+  }
+};
+</script>
+
+
+<style scoped>
+.quiz-heading {
+  margin: -40px 0 30px;
+  font-size: 30px;
+  text-shadow: 1px 1px 2px #020815;
+  line-height: 1.2;
+}
+
+.quiz-button {
+  color: #eee; /* Gray */
+  text-decoration: none;
+  cursor: pointer;
+  display: inline-block;
+  padding: 10px 30px;
+  border: 1px solid rgba(238, 238, 238, 0.3);
+  background: none;
+  transition: border-color 0.5s;
+}
+.quiz-button:hover {
+  border-color: #eee; /* Gray */
+}
+</style>
+
